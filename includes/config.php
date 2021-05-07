2ce0f794969a6a99d51237ed3616bc1d0178a816
odko <freeloff07@gmail.com>
2021-05-07 16:37:25 +0800

    s

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'bmz';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'ad_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '9.1';
+$config['installed'] = '1';
+?>
